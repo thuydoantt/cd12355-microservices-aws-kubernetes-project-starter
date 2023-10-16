bcb3d799d4e8e5ad2cd0683ea0c280f111eed28d
thuydoantt <thuy.doan@jobhopin.com>
2023-10-16 14:10:22 +0700

    update

--- analytics/app.py
+++ analytics/app.py
@@ -21,7 +21,7 @@ def health_check():
 @app.route("/readiness_check")
 def readiness_check():
     try:
-        db.session.execute('SELECT 1')
+        db.session.execute(text('SELECT 1'))
     except Exception as e:
         app.logger.error(e)
         return "failed", 500
